eb14a802273b3a242208aea26f20e3d9edd33bd8
lijiaming <1769041723@qq.com>
2022-11-11 11:47:50 +0800

    配置文件和安装包

---
+++ usr/local/redis/runcluster.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+./bin/redis-server /usr/local/redis-cluster/7001/7001.conf
+./bin/redis-server /usr/local/redis-cluster/7002/7002.conf
+./bin/redis-server /usr/local/redis-cluster/7003/7003.conf
+./bin/redis-server /usr/local/redis-cluster/7004/7004.conf
+./bin/redis-server /usr/local/redis-cluster/7005/7005.conf
+./bin/redis-server /usr/local/redis-cluster/7006/7006.conf
